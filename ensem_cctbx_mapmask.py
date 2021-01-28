ecb9b933896b906f504efbc2ece3f2e01c5fab4f
cm0ore <71089488+cm0ore@users.noreply.github.com>
2021-01-28 15:17:57 -0700

    ?

--- ensem_cctbx_mapmask.py
+++ ensem_cctbx_mapmask.py
@@ -1,3 +1,6 @@
+#Generates a shifted waterless_model, map, and mask and also shifts the original model by the same amount 
+#inputs are waterless_pdb_file and original_pdb_file
+
 import sys
 import os
 from iotbx.data_manager import DataManager     # load in DataManager
@@ -5,26 +8,31 @@ dm = DataManager()                             # Get an initialized version as d
 dm.set_overwrite(True)
 from iotbx.map_model_manager import map_model_manager      #   load in the map_model_manager
 mmm=map_model_manager()         #   get an initialized instance of the map_model_manager
-mm = mmm.map_manager()
-pdb_file = sys.argv[1]
-model = dm.get_model(pdb_file)
 
-#make a map within 1A of all residues in model with given file_name
-mmm.generate_map(file_name=pdb_file, box_cushion=1)
+no_water_pdb = sys.argv[1]
+model_filename = sys.argv[2] 
 
-file_basename = os.path.basename(pdb_file).split('.pdb')[0]
-mmm.write_map("%s.mrc" % file_basename)       #   write out a map in ccp4/mrc format
-mmm.write_model("%s.pdb" % file_basename)    #   write out a model in PDB format
-
-map_filename="%s.mrc" % file_basename
-mm = dm.get_real_map(map_filename)
-mm.shift_origin()
-mm.show_summary()
-
-
-mmm.create_mask_around_atoms(model=model)   #  create binary mask around atoms in the model
-
+#create map from waterless model and write new files 
+file_basename = os.path.basename(model_filename).split('.pdb')[0]
+mmm.generate_map(file_name=no_water_pdb, map_id='new_map', box_cushion=1)
+mmm.write_model("%s_shifted.pdb" % file_basename)
+mmm.write_map("%s_shifted_map.mrc" % file_basename) 
+mmm.create_mask_around_atoms()   #  create binary mask around atoms in the model
 mmm.apply_mask_to_maps()
-dm.write_real_map_file(mmm.map_manager()   ,filename="%s_masked.mrc" % file_basename) # masked
+dm.write_real_map_file(mmm.map_manager()   ,filename="%s_shifted_masked.mrc" % file_basename) # masked
+
+
+#shift coords of model_file with solvent
+model = dm.get_model("%s_shifted.pdb" % file_basename) #shifted and waterless
+with_water_model = dm.get_model(model_filename)
+sites_cart = with_water_model.get_sites_cart()          #      get coordinates of atoms in Angstroms
+from scitbx.matrix import col                # import a tool that handles vectors
+shift = tuple(i - j for i,j in zip(model.get_sites_cart()[0], with_water_model.get_sites_cart()[0])) 
+sites_cart += col(shift) 
+
+with_water_model.set_sites_cart(sites_cart)
+print(with_water_model.get_sites_cart()[0])            # print coordinate of first atom
+dm.write_model_file(with_water_model, "shifted_with_water.pdb", overwrite=True)
+
 
 
